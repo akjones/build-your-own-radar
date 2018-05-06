c55fbd454ec04b284306243596c8001126d941cd
Andrew Jones <andrew@andrewjones.id.au>
2018-05-07 09:12:00 +1000

    Add support for 5th ring

--- src/util/factory.js
+++ src/util/factory.js
@@ -25,7 +25,7 @@ const plotRadar = function (title, blips) {
 
     var rings = _.map(_.uniqBy(blips, 'ring'), 'ring');
     var ringMap = {};
-    var maxRings = 4;
+    var maxRings = 5;
 
     _.each(rings, function (ringName, i) {
         if (i == maxRings) {
